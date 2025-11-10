16d47ff94c4f05200218c3778826d354a051e331
Icarz <mr.rghay@email.com>
2025-11-10 14:12:06 +0100

    message logics are done

--- backend/controllers/message.controller.js
+++ backend/controllers/message.controller.js
@@ -24,11 +24,34 @@ export const sendMessage = async (req, res) => {
     if (newMessage) {
       conversation.messages.push(newMessage._id);
     }
+    // socket io implementation can be added here for real-time messaging //
+
     await Promise.all([conversation.save(), newMessage.save()]);
 
     res.status(200).json(newMessage);
   } catch (error) {
     console.log("Error in sendMessage controller:", error);
+    return res.status(500).json({ message: error.message });
+  }
+};
+
+// get message controller //
+
+export const getMessage = async (req, res) => {
+  try {
+    const { id: userToChatWith } = req.params;
+    const senderId = req.user._id;
+
+    const conversation = await Conversation.findOne({
+      participants: { $all: [senderId, userToChatWith] },
+    }).populate("messages");
+    if (!conversation) {
+      return res.status(200).json([]);
+    }
+    const messages = conversation.messages;
+    res.status(200).json(messages);
+  } catch (error) {
+    console.log("Error in getMessage controller:", error);
     return res.status(500).json({ message: error.message });
   }
 };
